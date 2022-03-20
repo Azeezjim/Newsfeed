91f2e66df3006590ff976843ba6c87e8dfc13203
Azeezjim <azeexjim@gmail.com>
2022-03-19 20:37:00 -1100

    !st commit newsfeed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Topnav from "./components/Topnav/Topnav";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1 className="text-blue-500"> Tailwind</h1> */}
+        <Topnav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Topnav/Topnav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { BiMenu } from 'react-icons/bi'
+
+export default function Topnav() {
+  return (
+    <div className="bg-red-600 w-full h-5">
+      
+    </div>
+  )
+}
+
